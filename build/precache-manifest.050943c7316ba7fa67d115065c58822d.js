10dfe98cafadb645ff88692bcbbd69c4ff5c1b8e
Marc Auberer <marc.auberer@chillibits.com>
2020-04-27 13:46:17 +0200

    Fix bug

---
+++ build/precache-manifest.050943c7316ba7fa67d115065c58822d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43219d0d48bfb963076ec1693343ff0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b1f7628993893a197cb",
+    "url": "/static/css/main.5c6e9a74.chunk.css"
+  },
+  {
+    "revision": "356567f3abd36157ef73",
+    "url": "/static/js/2.285c6fd1.chunk.js"
+  },
+  {
+    "revision": "f166c5f3552ef710cf1e3d6cc4d89e6b",
+    "url": "/static/js/2.285c6fd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1f7628993893a197cb",
+    "url": "/static/js/main.fd69ac9c.chunk.js"
+  },
+  {
+    "revision": "d4a9bc0589a39f50ab4d",
+    "url": "/static/js/runtime-main.e1e96c64.js"
+  }
+]);
